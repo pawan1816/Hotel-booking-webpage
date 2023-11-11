9470c6333242aad917a670965c9ecc52bf5ba0f9
pawan1816 <104552498+pawan1816@users.noreply.github.com>
2023-11-11 16:41:20 +0530

    Add files via upload

---
+++ Hotel managemant system/inc/footer.php
@@ -0,0 +1,30 @@
+<!-- footer -->
+<div class="container-fluid bg-white mt-5">
+  <div class="row">
+    <div class="col col-lg-4 p-4">
+     <h3 class="h-font fw-bold fs-3 mb-2">PJ HOTEL</h3>
+     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.Itaque consectetur, 
+       saepe quis iste vitae, ducimus quos commodi voluptates at minus natus iusto nesciunt
+       culpa labore. Rerum commodi consectetur earum consequuntur?</p>
+    </div>
+    <div class="col col-lg-4 p-4">
+     <h5 class="mb-3">links</h5>
+     <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Home</a><br>
+     <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Facilities</a><br>
+     <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Contact Us</a><br>
+     <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">About Us</a>
+    </div>
+    <div class="col col-lg-4 p-4">
+      <h class="mb-3 ">Follow Us</h><br>
+      <a href="#" class="d-inline-block text-dark text-decoration-none mb-2">
+        <i class="bi bi-twitter me-1"></i>twitter</a><br>
+      <a href="#" class="d-inline-block text-dark text-decoration-none mb-2">
+        <i class="bi bi-facebook me-1"></i>facebook</a><br>
+      <a href="#" class="d-inline-block text-dark text-decoration-none">
+        <i class="bi bi-instagram me-1"></i>instagram</a>
+    </div>
+  </div>
+</div>
+<h6 class="text-center bg-dark text-white p-3 m=0">design by pawan </h6>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
